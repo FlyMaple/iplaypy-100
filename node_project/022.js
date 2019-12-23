f07b5e88f2e33192c760dd99ad28539cf4e0a40a
Skye.Wu <skye.wu@zyxel.com.tw>
2019-12-23 11:30:14 +0800

    022
    Signed-off-by: Skye.Wu <skye.wu@zyxel.com.tw>

--- node_project/022.js
+++ node_project/022.js
@@ -5,3 +5,34 @@
 // 已抽籤決定比賽名單。
 
 // 問題：有人向隊員打聽比賽的名單。a說他不和x比，c說他不和x,z比，請編程序找出三隊賽手的名單。
+
+var teamA = ['a', 'b', 'c'];
+var teamB = ['x', 'y', 'z'];
+
+function calculate() {
+  var new_list = [];
+  for (var memberA of teamA) {
+    if (teamB.length === 1) {
+      console.log(`${memberA} <==> ${teamB[0]}`);
+      teamB = [];
+      break;
+    }
+    if (memberA === 'a') {
+      new_list = teamB.filter(x => x !== 'x');
+    }
+    if (memberA === 'c') {
+      new_list = teamB.filter(x => x !== 'x' && x !== 'z');
+    }
+    if (new_list.length === 1) {
+      console.log(`${memberA} <==> ${new_list[0]}`);
+      teamB = teamB.filter(x => x !== new_list[0]);
+    }
+  }
+}
+
+while (true) {
+  if (teamB.length === 0) {
+    break;
+  }
+  calculate();
+}
--- python_project/022.py
+++ python_project/022.py
@@ -5,3 +5,30 @@
 # 已抽籤決定比賽名單。
 
 # 問題：有人向隊員打聽比賽的名單。a說他不和x比，c說他不和x,z比，請編程序找出三隊賽手的名單。
+
+teamA = ['a', 'b', 'c']
+teamB = ['x', 'y', 'z']
+
+
+def calculate():
+    global teamA, teamB
+
+    new_list = []
+    for memberA in teamA:
+        if len(teamB) == 1:
+            print('%s <==> %s' % (memberA, teamB[0]))
+            teamB = []
+            break
+        if memberA == 'a':
+            new_list = list(filter(lambda x: x != 'x', teamB))
+        if memberA == 'c':
+            new_list = list(filter(lambda x: x not in ['x', 'z'], teamB))
+        if len(new_list) == 1:
+            print('%s <==> %s' % (memberA, new_list[0]))
+            teamB = list(filter(lambda x: x != new_list[0], teamB))
+
+
+while True:
+    if len(teamB) == 0:
+        break
+    calculate()
