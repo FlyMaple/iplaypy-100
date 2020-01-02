66ee068758f02ad61f740f3ae9be8e996cb944f7
Skye.Wu <rottensociety1987@gmail.com>
2020-01-02 22:33:23 +0800

    033
    Signed-off-by: Skye.Wu <rottensociety1987@gmail.com>

---
+++ node_project/033.js
@@ -0,0 +1,7 @@
+// Python join()逗號分割練習題要求，按逗號分隔列表。
+
+var l = [1, 2, 3, 4, 5]
+
+console.log(l);
+var l2 = l.join(', ');
+console.log(l2);
---
+++ python_project/033.py
@@ -0,0 +1,7 @@
+# Python join()逗號分割練習題要求，按逗號分隔列表。
+
+l = [1, 2, 3, 4, 5]
+
+print(l)
+l2 = ', '.join(str(i) for i in l)
+print(l2)
